8c50ba5836c50d80c47fc33804400faf57bb9efa
jansenokasaki <jansencruzyuki@gmail.com>
2025-02-17 23:45:05 +0000

    initial commit

---
+++ ex01/src/app/page.js
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+function Texto1() {
+  return <p>Um outro paragráfo de texto.</p>;
+}
+
+export default function Home() {
+const aula = 2;
+return (
+    <div>
+      <h1>Hello Jansen</h1>
+      <p>Essa é a nossa aula {aula}ª de React</p>
+      <Texto1 />
+      <Texto1 />
+    </div>
+  );
+}
