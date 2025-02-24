fb58e3cc1f957e09f9d96a59d35a0b742512d2b4
jansenokasaki <jansencruzyuki@gmail.com>
2025-02-24 23:48:34 +0000

    .

--- ex01/src/app/page.js
+++ ex01/src/app/page.js
@@ -1,20 +1,24 @@
-import Image from "next/image";
+Function Texto1() {
 
 export function Texto1() {
   return <p>Um outro paragráfo de texto.</p>;
 }
 
 export default function Home() {
+const [gide, setHide] = useState(false);
 const aula = 2;
 return (
     <div>
       <h1>Hello Jansen</h1>
       <p>Essa é a nossa aula {aula}ª de React</p>
+      {hide && (
+       <>
       <Texto1 />
       <Texto1 />
+       </>
+      )}
     </div>
-  );
-}
-
+    };
+  }
 
 // import Home, { Texto1} from './page'; 
