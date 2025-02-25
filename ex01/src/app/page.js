9ed32bc2becdfcf0f4af2a0cd097e232cc6e935e
jansenokasaki <jansencruzyuki@gmail.com>
2025-02-25 00:27:32 +0000

    .

--- ex01/src/app/page.js
+++ ex01/src/app/page.js
@@ -8,12 +8,15 @@ function Texto1() {
  
 export default function Home() {
   const [hide, setHide] = useState(false);
+  const [numAleat, setNumAleat] = useState (gerarNumAleat());
   const aula = 2;
   return (
     <div>
     <h1>Hello Lua!</h1>
       <p> {aula}ª aula  de React </p>
       <hr />
+      <button onClick={() => setNumAleat(Math.floor(Math.random() * 100))
+       Aleatório: {NumAleat}
       <button onClick={() => setHide(!hide)}>{hide ? "Abrir" : "Fechar"}</button>
       <hr />
       {!hide && (
