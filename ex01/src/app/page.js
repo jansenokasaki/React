2cd6d218ac3cdb2305fe4f14ced2f36155992849
jansenokasaki <jansencruzyuki@gmail.com>
2025-02-25 00:29:00 +0000

    .

--- ex01/src/app/page.js
+++ ex01/src/app/page.js
@@ -3,28 +3,34 @@
 import { useState } from "react";
  
 function Texto1() {
-  return <p>Um parágrafo de texto</p>;
+  return <p>Um outro parágrafo de texto!</p>;
+}
+ 
+function gerarNumAleat() {
+  return Math.floor(Math.random() * 100) + 1;
 }
  
 export default function Home() {
   const [hide, setHide] = useState(false);
-  const [numAleat, setNumAleat] = useState (gerarNumAleat());
+  const [numAleat, setNumAleat] = useState(gerarNumAleat());
   const aula = 2;
   return (
-    <div>
-    <h1>Hello Jansen!</h1>
-      <p> {aula}ª aula  de React </p>
-      <hr />
-      <button onClick={() => setNumAleat(Math.floor(Math.random() * 100))
-       Aleatório: {NumAleat}
-      <button onClick={() => setHide(!hide)}>{hide ? "Abrir" : "Fechar"}</button>
-      <hr />
+<div>
+<h1>Hello Jansen</h1>
+<p>Essa é a nossa {aula}ª aula de React!</p>
+<hr />
+<button onClick={() => setNumAleat(gerarNumAleat())}>
+        Aletatório: {numAleat}
+</button>
+<hr />
+<button onClick={() => setHide(!hide)}>{hide ? "Show" : "Hide"}</button>
+<hr />
       {!hide && (
-        <>
-          <Texto1 />
-          <Texto1 />
-        </>
+<>
+<Texto1 />
+<Texto1 />
+</>
       )}
-    </div>
-  )
+</div>
+  );
 }
