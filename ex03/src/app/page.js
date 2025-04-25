37f573e2434e4788911b67626eb55013e007a330
jansenokasaki <jansencruzyuki@gmail.com>
2025-04-25 00:00:22 +0000

    Correções

--- ex03/src/app/page.js
+++ ex03/src/app/page.js
@@ -4,9 +4,9 @@ export default function Home() {
       <h1 className="text-2xl font-bold">Conversores Inteligentes</h1>
       <p>Este app converte valores automaticamente e em tempo real:</p>
       <ul className="list-disc ml-6 space-y-1">
-        <li><a className="text-blue-500 underline" href="/currency">Conversor de Dólar/Real</a></li>
-        <li><a className="text-blue-500 underline" href="/temperature">Conversor de Temperatura</a></li>
-        <li><a className="text-blue-500 underline" href="/length">Conversor de Comprimento</a></li>
+        <li><a className="text-blue-500 underline" href="/CurrencyaConverter">Conversor de Dólar/Real</a></li>
+        <li><a className="text-blue-500 underline" href="/TemperatureConverter">Conversor de Temperatura</a></li>
+        <li><a className="text-blue-500 underline" href="/LengthConverter">Conversor de Comprimento</a></li>
       </ul>
     </main>
   );
