de542cc4d2cb49e547c154824095d72344459c80
jansenokasaki <jansencruzyuki@gmail.com>
2025-04-25 00:01:37 +0000

    Mais uma correção no href

--- ex03/src/app/page.js
+++ ex03/src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
       <h1 className="text-2xl font-bold">Conversores Inteligentes</h1>
       <p>Este app converte valores automaticamente e em tempo real:</p>
       <ul className="list-disc ml-6 space-y-1">
-        <li><a className="text-blue-500 underline" href="/CurrencyaConverter">Conversor de Dólar/Real</a></li>
+        <li><a className="text-blue-500 underline" href="/CurrencyConverter">Conversor de Dólar/Real</a></li>
         <li><a className="text-blue-500 underline" href="/TemperatureConverter">Conversor de Temperatura</a></li>
         <li><a className="text-blue-500 underline" href="/LengthConverter">Conversor de Comprimento</a></li>
       </ul>
