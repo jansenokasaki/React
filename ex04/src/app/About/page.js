69bae42e517396da45b4c740e6c684a63d608727
Jansen Okasaki <jansencruzyuki@gmail.com>
2025-06-02 22:48:18 +0000

    Node modules

--- ex04/src/app/About/page.js
+++
@@ -1,9 +0,0 @@
-export default function About() {
-    return (
-      <main className="p-6">
-        <h1 className="text-2xl font-bold mb-4">Sobre o Desenvolvedor</h1>
-        <p>Olá! Meu nome é [Jansen], sou estudante de Sistemas para Internet na UNICAP e residente no Porto Digital. Este app foi feito como projeto de aprendizado.</p>
-      </main>
-    );
-  }
-  
